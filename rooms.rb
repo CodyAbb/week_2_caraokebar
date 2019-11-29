1f12dcccf2243e12d9292a94d779e9be00c07ad9
Cody Abbott <cody.abbott16@gmail.com>
2019-11-29 15:36:37 +0000

    fixed guest adding problem

---
+++ rooms.rb
@@ -0,0 +1,18 @@
+class Room
+
+  attr_reader :occupants, :playlist
+
+  def initialize(occupants, playlist)
+    @occupants = []
+    @playlist = []
+  end
+
+  def add_guest_to_room(guest)
+    @occupants.concat(guest)
+  end
+
+  def add_songs_to_playlist(song)
+    @playlist << song
+  end
+
+end
---
+++ specs/rooms_spec.rb
@@ -0,0 +1,30 @@
+require('minitest/autorun')
+require('minitest/reporters')
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+require_relative('../rooms')
+require_relative('../guests')
+require_relative('../songs')
+
+class RoomTest < Minitest::Test
+
+  def setup
+
+    @guest1 = Guest.new("Mark", 10, "Rocketman")
+    @guest2 = Guest.new("David", 20, "Purple Rain")
+    @guests = [@guest1, @guest2]
+
+    @song1 = Song.new("Angels", "Robbie Williams")
+    @song2 = Song.new("Say it ain't so", "Weezer")
+    @song3 = Song.new("Rocketman", "Elton John")
+    @songs = [@song1, @song2, @song3]
+    @false_songs = [@song1, @song2]
+
+    @room = Room.new(@guests, @songs)
+  end
+
+  def test_num_of_guests_in_room
+    @room.add_guest_to_room(@guests)
+    assert_equal(2, @room.occupants.length)
+  end
+
+end
