a657c0fcec7d71bdc70a974e972055b95b393efc
Cody Abbott <cody.abbott16@gmail.com>
2019-11-29 16:20:58 +0000

    set entry initialization for the bar and checking customers money

--- karaoke_bar.rb
+++ karaoke_bar.rb
@@ -1,8 +1,21 @@
 class KaraokeBar
 
+  attr_reader :name, :entry_cost
 
-  def initialize(name)
+  def initialize(name, entry_cost, till, rooms)
     @name = name
+    @entry_cost = entry_cost
+    @rooms = []
+    @till = till
+  end
+
+
+  def customer_money_check(guest)
+    if guest.wallet >= entry_cost
+      return true
+    else
+      return false
+    end
   end
 
 end
--- specs/karaoke_bar.spec.rb
+++ specs/karaoke_bar.spec.rb
@@ -8,6 +8,41 @@ require_relative('../songs')
 
 class KaraokeBarTest < Minitest::Test
 
-  
+  def setup
+
+    @guest1 = Guest.new("Mark", 10, "Rocketman")
+    @guest2 = Guest.new("David", 20, "Purple Rain")
+    @guests = [@guest1, @guest2]
+
+    @guest3 = Guest.new("Sam", 3, "Song 2")
+
+    @song1 = Song.new("Angels", "Robbie Williams")
+    @song2 = Song.new("Say it ain't so", "Weezer")
+    @song3 = Song.new("Rocketman", "Elton John")
+    @songs = [@song1, @song2, @song3]
+    @false_songs = [@song1, @song2]
+
+    @room1 = Room.new(@guest1, @songs)
+
+    @karaoke_bar = KaraokeBar.new("SugarCube", 5, 100, @room1)
+  end
+
+  def test_get_bar_name
+    assert_equal("SugarCube", @karaoke_bar.name)
+  end
+
+  def test_get_entry_amount
+    assert_equal(5, @karaoke_bar.entry_cost)
+  end
+
+  def test_check_customer_can_afford_entry_cost
+    test_value = @karaoke_bar.customer_money_check(@guest1)
+    assert_equal(true, test_value)
+  end
+
+  def test_check_customer_can_afford_entry_cost_false
+    test_value = @karaoke_bar.customer_money_check(@guest3)
+    assert_equal(false, test_value)
+  end
 
 end
